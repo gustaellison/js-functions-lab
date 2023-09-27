3585e8e8a19138205505f6c00fb67d797271b39d
Augusta Ellison <gustaellison@gmail.com>
2023-09-27 15:30:10 -0400

    learnign about functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 console.log('its all connected')
+
+//defining a function
+// declaring function
+//creating the reusable block of code
+// to be used at a later time in the program
+function sayHello(personsName) {
+    // write the code to say hello here
+    console.log(`Hello  ${personsName}`)
+}
+//execute the function
+//invoke the function
+//call the function
+
+// write the function name ()
+sayHello('Jim'); //runs the block of code defined by the sayHello function
+sayHello('Augusta');
+sayHello('Melissa');
+sayHello('Brad');
+
+function sayGoodbye(phrase, person) {
+    console.log(`${phrase}, ${person}`)
+}
+    
+sayGoodbye('Adios', 'Laura')
+sayGoodbye('Chao', 'Becky')
+sayGoodbye('Goodbye', 'Chad')
+sayGoodbye('Peace', 'Frankie')
