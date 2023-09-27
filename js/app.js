67546052714f6f277f41fb88022d7ef0fe22b101
Augusta Ellison <gustaellison@gmail.com>
2023-09-27 18:30:09 -0400

    number 5 finished

--- js/app.js
+++ js/app.js
@@ -65,7 +65,12 @@ function maxOfTwoNumbers(x, y) {
   
   console.log(maxOfTwoNumbers(3, 9));
   
-  // 2.
+  // 2. 
+  /*Define a function, as a function expression, maxOfThree 
+  that takes three numbers as arguments and returns the largest 
+  of them. Again, the Math.max method is not allowed.
+*/
+
 function maxOfThree(x, y, z) {
     if ( x >= y && x >= z){
         return x;
@@ -79,6 +84,10 @@ function maxOfThree(x, y, z) {
 console.log(maxOfThree(6, 4, 10))
 
 // 3.
+  /*Define a function, as a function declaration, isCharAVowel that 
+  takes a character as an argument and returns true if it is a vowel, 
+  false otherwise.
+*/
 function isCharAVowel(letter) {
     if (letter === 'a' || letter ==='e' || letter ==='i' || letter ==='o' || letter ==='u') {
         return 'it is a vowel';
@@ -92,12 +101,61 @@ let testLetter = "b"
 console.log(isCharAVowel(testLetter))
 
 // 4. 
+  /*Define a function, as a function expression, sumArray that 
+  takes an array of numbers and returns the sum of those numbers.
+   For example, sumArray([2, 4, 5]); would return 11
+*/
 
-const arr = [3, 4, 6, 10]
-let total = 0
 
- arr.forEach(function (nums) {
-    total = total + nums
- })
- console.log(total)
+let sumArray = function(arr) {
+    let total = 0
+    for (let i = 0; i < arr.length - 1; i++){
+        total += arr[i]
+    }
+    return total
+}
+console.log(sumArray([3, 4, 6, 10]))
+console.log(sumArray([5, 7, 9]))
+
+
+//  arr.forEach(function (nums) {
+//     total += nums
+//  })
+
+
+
+// 5. 
+/* Define a function, as a function declaration, multiplyArray that 
+takes an array of numbers and returns the product those numbers. 
+For example, multiplyArray([2, 4, 5]); would return 40.
+*/
+
+function multiplyArray (arr) {
+    let total = 1
+    arr.forEach(function(num){
+        total *= num
+    })
+    return total
+}
+console.log(multiplyArray([10, 10, 8]))
+
+// 6 Define a function, as a function expression, numArgs 
+//that returns the number of arguments passed to the function when called.
+let numArgs = function()
+
+
+
+// 9.
+/* Define a function, as a function declaration, stringsLongerThan 
+that takes an array of strings and a number as arguments; and returns an
+array of the strings that are longer than the number passed in. For example, 
+stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return 
+["hello", "morning"].
+*/
+
+function stringsLongerThan(strArray, num){
+    for (let i = 0; i < strArray.length - 1; i++){
+    
+}    
+}
 
