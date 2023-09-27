8c370eb295b1af52d276bb3f811188cbe48833c4
Augusta Ellison <gustaellison@gmail.com>
2023-09-27 15:55:53 -0400

    learned about console log vs return

--- js/app.js
+++ js/app.js
@@ -20,9 +20,33 @@ sayHello('Brad');
 
 function sayGoodbye(phrase, person) {
     console.log(`${phrase}, ${person}`)
+    //return `${phrase}, ${person}` //I think this would need to mirror the getPlayerUserName with defined variables and calling functions for each
+
 }
     
 sayGoodbye('Adios', 'Laura')
 sayGoodbye('Chao', 'Becky')
 sayGoodbye('Goodbye', 'Chad')
 sayGoodbye('Peace', 'Frankie')
+
+function add(x , y){
+    return x + y + ' is the answer.'
+}
+
+console.log(add(2, 2), 'Look at this')
+
+function getPlayerUserName(username){
+    return username.toUpperCase();
+}
+//before they join the game we dont know their user name
+let playerOne;
+let playerTwo;
+let playerThree;
+let playerFour;
+
+//after the join, we set the players name
+
+playerOne = getPlayerUserName('Augusta')
+playerTwo = getPlayerUserName('Jon')
+playerThree = getPlayerUserName('Alyssa')
+playerFour = getPlayerUserName('Dilly')
