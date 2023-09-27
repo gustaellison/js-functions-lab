73754f78311e4403f24b437ce19448d0da16f3e7
Augusta Ellison <gustaellison@gmail.com>
2023-09-27 16:42:26 -0400

    problems 2 and 3 completed

--- js/app.js
+++ js/app.js
@@ -50,3 +50,42 @@ playerOne = getPlayerUserName('Augusta')
 playerTwo = getPlayerUserName('Jon')
 playerThree = getPlayerUserName('Alyssa')
 playerFour = getPlayerUserName('Dilly')
+
+// 1.
+function maxOfTwoNumbers(x, y) {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+    
+    // or more "elegantly" using the fantastic ternary expression!
+    // return  x >= y ? x : y;
+  }
+  
+  console.log(maxOfTwoNumbers(3, 9));
+  
+  // 2.
+function maxOfThree(x, y, z) {
+    if ( x >= y && x >= z){
+        return x;
+    } else if ( y >= x && y >= z){
+        return y;
+    } else {
+        return z;
+    }
+}
+
+console.log(maxOfThree(6, 4, 10))
+
+// 3.
+function isCharAVowel(letter) {
+    if (letter === 'a' || letter ==='e' || letter ==='i' || letter ==='o' || letter ==='u') {
+        return 'it is a vowel';
+    } else {
+        return 'it is not a vowel'
+            }
+}
+    
+
+console.log(isCharAVowel('a'))
